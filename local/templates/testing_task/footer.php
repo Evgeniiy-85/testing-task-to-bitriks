b8b6a42c426236adb37360fe95d8ce4a4e8fbb4e
Evgeniy <Evgeniy@DESKTOP-UHD36O5>
2019-04-03 05:20:10 -0300

    Правки

--- local/templates/testing_task/footer.php
+++ local/templates/testing_task/footer.php
@@ -3,7 +3,8 @@
 <footer>
 	<div class="container">
 		<div class="footer-info">
-			
+			<div><span>©2003-2009 dfmc.com.cn All rights reserved.</span></div>
+			<div><span>Разработка сайта — SUPPORTix</span></div>
 		</div>
 	</div>
 </footer>
